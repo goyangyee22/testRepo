8bbb68136c855604e5cb7722a1c2fac9a295ab4d
LeeJh <4447447@naver.com>
2024-07-10 07:03:29 +0900

    nodejs

--- react/01-dice_game/src/App.js
+++ react/01-dice_game/src/App.js
@@ -1,108 +1,40 @@
+import { useEffect, useState } from "react"
 import "./App.css";
-import "./HandIcon.css";
-import HandIcon from "./HandIcon";
-import reset from "./assets/ic-reset.svg";
-import HandButton from "./HandButton";
-import { useState } from "react";
-import { compareHand, generateRandomHand } from "./utils";
+import Cleanup from "./Cleanup";
+import ToDoList from "./TodoList";
 
-function getResult(comparison) {
-  if (comparison > 0) return "승리";
-  if (comparison < 0) return "패배";
-  return "무승부";
+function App(){
+const [num, setNum] = useState(0);
+const [text, setText] = useState("");
+
+const handleClick = () => {
+  setNum(num+1);
 }
 
-function App() {
-  const [hand, setHand] = useState("rock");
-  const [otherHand, setOtherHand] = useState("rock");
-  const [score, setScore] = useState(0);
-  const [otherScore, setOtherScore] = useState(0);
-  const [bet, setBet] = useState(1);
-  const [gameHistory, setGameHistory] = useState([]);
-  const [isWin, setIsWin] = useState(0);
+useEffect(() => {
+  console.log("나는 컴포넌트가 최초 렌더링될 때 실행되는 uef야.");
+}, []);
 
-  const handleButtonClick = (value) => {
-    setHand(value);
-    // generateRandomHand()에 대해 알아보자
-    const nextOtherHand = generateRandomHand();
-    setOtherHand(nextOtherHand);
-    const comparison = compareHand(value, nextOtherHand);
-    setIsWin(comparison);
-    if (comparison > 0) setScore(score + bet);
-    if (comparison < 0) setOtherScore(otherScore + bet);
-    const result = getResult(comparison);
-    setGameHistory([...gameHistory, result]);
-  };
+useEffect(() => {
+  console.log("나는 count가 변경될 때 실행되는 uef야.");
+}, [num]);
 
-  // 배점 변경
-  const handleBetChange = (e) => {
-    let num = Number(e.target.value);
-    if (num > 9) num = num % 10;
-    if (num < 1) num = 1;
-    num = Math.floor(num);
-    setBet(num);
-  };
+useEffect(() => {
+  console.log("나는 text가 변경될 때 실행되는 uef야.");
+}, [text]);
 
-  // 초기화
-  const handleClearClick = () => {
-    setHand("rock");
-    setOtherHand("rock");
-    setScore(0);
-    setOtherScore(0);
-    setBet(1);
-    setGameHistory([]);
-    setIsWin(0);
-  };
-
-  return (
-    <div className="App">
-      <h1 className="App-heading">가위바위보</h1>
-      <img className="App-reset" src={reset} onClick={handleClearClick} />
-      <div className="App-scores">
-        <div className="Score">
-          <div className="Score-num">{score}</div>
-          <div className="Score-name">나</div>
-        </div>
-        <div className="App-versus">:</div>
-        <div className="Score">
-          <div className="Score-num">{otherScore}</div>
-          <div className="Score-name">상대</div>
-        </div>
-      </div>
-      <div className="Box App-box">
-        {/* 가위바위보 내는 곳 */}
-        <div className="App-hands">
-          <div
-            className={`Hand ${isWin == 0 ? "" : isWin == 1 ? "winner" : ""}`}
-          >
-            <HandIcon value={hand} className="Hand-icon" />
-          </div>
-          <div className="App-versus">VS</div>
-          <div
-            className={`Hand ${isWin == 0 ? "" : isWin == 1 ? "" : "winner"}`}
-          >
-            <HandIcon value={otherHand} className="Hand-icon" />
-          </div>
-        </div>
-        {/* 배점 */}
-        <div className="App-bet">
-          <span>배점</span>
-          <input type="number" min={1} max={9} onChange={handleBetChange} />
-          <span>배</span>
-        </div>
-        {/* 기록 */}
-        <div className="App-history">
-          <h2>승부기록</h2>
-          <p>{gameHistory.join(", ")}</p>
-        </div>
-      </div>
-      <div>
-        <HandButton value="rock" onClick={handleButtonClick} />
-        <HandButton value="scissor" onClick={handleButtonClick} />
-        <HandButton value="paper" onClick={handleButtonClick} />
-      </div>
+  return(
+    <div>
+      <input />
+      <h2>입력한 값: </h2>
+      <h1></h1>
+      <button onClick={handleClick}>Click me</button>
+      <hr />
+      <Cleanup />
+      <hr />
+      <ToDoList />
     </div>
-  );
+  )
 }
 
 export default App;
