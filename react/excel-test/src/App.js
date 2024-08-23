06f096d308e24446721f037891ddaedca515e544
yeeun <48862667+goyangyee22@users.noreply.github.com>
2024-08-23 12:16:56 +0900

    update

--- react/excel-test/src/App.js
+++ react/excel-test/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Papa from "papaparse";
-import { Bar } from "react-chartjs-2";
+import { Bar, Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
---
+++ react/geolocation-test/src/App.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [position, setPosition] = useState({ lat: null, lon: null });
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // 위치 정보를 가져오는 함수
+    const fetchLocation = () => {
+      if (navigator.geolocation) {
+        //  navigator.geolocation.getCurrentPosition(
+        const watchId = navigator.geolocation.watchPosition(
+          (position) => {
+            setPosition({
+              lat: position.coords.latitude,
+              lon: position.coords.longitude,
+            });
+          },
+          (error) => {
+            switch (error.code) {
+              case error.PERMISSION_DENIED:
+                setError("사용자가 위치 권한을 거부했습니다.");
+                break;
+              case error.POSITION_UNAVAILABLE:
+                setError("위치 정보를 사용할 수 없습니다.");
+                break;
+              case error.TIMEOUT:
+                setError("위치 정보 요청 시간이 초과되었습니다.");
+                break;
+              case error.UNKNOWN_ERROR:
+                setError("알 수 없는 오류가 발생했습니다.");
+                break;
+              default:
+                setError("알 수 없는 오류가 발생했습니다.");
+                break;
+            }
+          }
+        );
+
+        return () => {
+          navigator.geolocation.clearWatch(watchId);
+        };
+      } else {
+        setError("Geolocation API를 지원하지 않는 브라우저입니다.");
+      }
+    };
+
+    fetchLocation();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>현재 위치</h1>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {position.lat && position.lon ? (
+        <div>
+          <p>
+            위도: {position.lat}
+            <p>경도: {position.lon}</p>
+          </p>
+        </div>
+      ) : (
+        <p>위치 정보를 가져오는 중...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
