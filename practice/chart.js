dc8f15bf9d48d945784360241e2993ac7ca3f5a7
LeeJh <48862667+goyangyee22@users.noreply.github.com>
2024-10-21 16:34:00 +0900

    update

---
+++ practice/chart.js
@@ -0,0 +1,45 @@
+// CSV 파일 경로
+const path = "/practice/chart.csv";
+
+// CSV 데이터 파싱
+Papa.parse(path, {
+    download: true,
+    header: true,
+    complete: function(results){
+        const years = ['2019', '2020', '2021', '2022', '2023'];
+
+        const birthData = years.map(year => results.data[0][year]);
+        const fertilityData = years.map(year => results.data[1][year]);
+
+        const ctx = document.getElementById("chart").getContext("2d");
+        new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: years,
+                datasets: [
+                    {
+                        label: "출생아수",
+                        data: birthData.map(Number),
+                        backgroundColor: "rgba(75, 192, 192, 0.2)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1
+                    },
+                    {
+                        label: '합계출산율',
+                        data: fertilityData.map(Number),
+                        backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                        borderColor: 'rgba(153, 102, 255, 1)',
+                        borderWidth: 1
+                    },
+                ]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+});
